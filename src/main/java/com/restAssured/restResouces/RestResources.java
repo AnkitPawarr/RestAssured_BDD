59db36765e6f91865564ce61cb59f8aac6fd1025
Ankit Pawar <pawarankit252@gmail.com>
2024-12-27 19:27:35 +0530

    Modifications in RestResources

--- src/main/java/com/restAssured/restResouces/RestResources.java
+++ src/main/java/com/restAssured/restResouces/RestResources.java
@@ -11,13 +11,16 @@ import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import static com.restAssured.hooks.Base.log;
-import static org.hamcrest.Matchers.lessThan;
 import static org.hamcrest.number.OrderingComparison.lessThanOrEqualTo;
 
 public class RestResources {
 
-    public RequestSpecification setUpRequest(String baseUri, Map<String, Object> headers, Map<String, Object> queryParams, Map<String, Object> pathParams, ContentType contentType) {
-        RequestSpecification request = RestAssured.given().log().all().baseUri(baseUri).contentType(contentType).accept(contentType);
+    public RequestSpecification setUpRequest(String baseUri, Map<String, Object> headers, Map<String, Object> queryParams,
+                                             Map<String, Object> pathParams, ContentType contentType) {
+        RequestSpecification request = RestAssured.given().log().all()
+                .baseUri(baseUri)
+                .contentType(contentType)
+                .accept(contentType);
         if (headers != null) {
             request.headers(headers);
         }
@@ -32,45 +35,53 @@ public class RestResources {
     }
 
     public Response get(RequestSpecification request, String endpoint) {
-        return request.log().all().when().get(endpoint);
+        return request.log().all()
+                .when().get(endpoint);
     }
 
     public Response post(RequestSpecification request, String endpoint, String payloadFilePath) {
         File payloadFile = new File(payloadFilePath);
-        return request.body(payloadFile).log().all().when().post(endpoint);
+        return request.body(payloadFile).log().all()
+                .when().post(endpoint);
     }
 
     public Response put(RequestSpecification request, String endpoint, String payloadFilePath) {
         File payloadFile = new File(payloadFilePath);
-        return request.body(payloadFile).log().all().when().put(endpoint);
+        return request.body(payloadFile).log().all()
+                .when().put(endpoint);
     }
 
     public Response patch(RequestSpecification request, String endpoint, String payloadFilePath) {
         File payloadFile = new File(payloadFilePath);
-        return request.body(payloadFile).log().all().when().patch(endpoint);
+        return request.body(payloadFile).log().all()
+                .when().patch(endpoint);
     }
 
     public Response delete(RequestSpecification request, String endpoint, String payloadFilePath) {
         File payloadFile = new File(payloadFilePath);
-        return request.body(payloadFile).log().all().when().delete(endpoint);
+        return request.body(payloadFile).log().all()
+                .when().delete(endpoint);
     }
 
     public void doVerifyStatusCode(Response response, int statusCode) {
-        response.then().log().all().assertThat().statusCode(statusCode);
+        response.then().log().all()
+                .assertThat().statusCode(statusCode);
     }
 
     public void doVerifySchema(Response response, String schemaFilePath) {
         try {
-            response.then().log().all().assertThat().body(JsonSchemaValidator.matchesJsonSchemaInClasspath(schemaFilePath));
+            response.then().log().all()
+                    .assertThat().body(JsonSchemaValidator.matchesJsonSchemaInClasspath(schemaFilePath));
         } catch (RuntimeException e) {
             log.error("JSON Schema validation FAILED.", e.getCause());
             throw new RuntimeException("JSON Schema validation FAILED.", e);
         }
     }
 
-    public <T> T doVerifyValueInResponse(Response response, String path, Class<T> type) {
+    public <T> T doExtractValueInResponse(Response response, String path, Class<T> type) {
         return response.jsonPath().getObject(path, type);
         /*
+        Example -> String email = extractValue(response, "data.user.contact.email", String.class);
         <T> in method declaration is a generic type parameter,
         which makes the method flexible and reusable for extracting values of any data type from the response.
         <T> is a placeholder for a type that will be specified when the method is called.
@@ -91,6 +102,10 @@ public class RestResources {
     }
 
     public void doVerifyResponseTime(Response response) {
-        response.then().log().all().time(lessThanOrEqualTo(200L), TimeUnit.MICROSECONDS);
+        response.then().log().all()
+                .time(lessThanOrEqualTo(200L), TimeUnit.MICROSECONDS);
+        /*We can use lessThan() method as well
+         The time should be in Long and not an Integer */
+
     }
 }
