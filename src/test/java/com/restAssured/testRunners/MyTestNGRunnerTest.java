55cd75486fc1a495355da5267b9a8b6f784000a6
Ankit Pawar <pawarankit252@gmail.com>
2024-12-28 13:05:14 +0530

    Added lombok in pom and Tags comments in runner

--- src/test/java/com/restAssured/testRunners/MyTestNGRunnerTest.java
+++ src/test/java/com/restAssured/testRunners/MyTestNGRunnerTest.java
@@ -8,11 +8,17 @@ import io.cucumber.testng.CucumberOptions;
         glue = {"com.restAssured.stepDefinition", "com.restAssured.hooks"},
         features = {"src/test/resources/featureFiles"},
         monochrome = true,
+        /*
+        AND condition: Use tags = "@tag1 and @tag2" to run scenarios with both tags.
+        OR condition: Use tags = "@tag1 or @tag2" to run scenarios with either tag.
+        NOT condition: Use tags = "not @tag1" to exclude scenarios with a specific tag
+        Complex condition: tags = "(@smoke or @regression) and not @wip"
+        */
         plugin = {
                 "pretty",
                 "html:target/cucumber-reports/cucumber.html",
                 "json:target/cucumber-reports-json/cucumber.json",
-                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
         }
 )
 
